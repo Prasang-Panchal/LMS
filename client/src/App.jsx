585bb0033863dcd73c47049a87a89247a7b6e8d8
Prasang Panchal <prasangpanchal07@gmail.com>
2025-02-26 18:27:07 +0530

    create hero section in studentpage also changes in tailwindcss version and navbar changes and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useMatch } from "react-router-dom";
 import Home from "./pages/student/Home";
 import CoursesList from "./pages/student/CoursesList";
 import CourseDetails from "./pages/student/CourseDetails";
@@ -14,9 +14,12 @@ import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
 import Navbar from "./components/student/Navbar";
 
 const App = () => {
+  const isEducatorRoute = useMatch("/educator/*");
+
   return (
     <div className="text-default min-h-screen bg-white">
-      <Navbar />
+      {!isEducatorRoute && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CoursesList />} />
@@ -40,4 +43,4 @@ const App = () => {
 
 export default App;
 
-//1:01:00
+//1:31:49
